7f224959e8e00160703b533d2f365f30127516f9
Musstavo <mohdtantour@gmail.com>
2026-01-29 23:56:41 +0300

    cnn on steriods be like

--- vision.py
+++ vision.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from helper_functions import accuracy_fn
 from timeit import default_timer as timer
 from tqdm.auto import tqdm
+import random
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -36,7 +37,7 @@ for i in range(1, rows * cols + 1):
     plt.title(class_names[label])
     plt.axis(False)
 
-BATCH_SIZE = 512
+BATCH_SIZE = 32
 
 train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
@@ -58,16 +59,6 @@ class FashionMNIST(nn.Module):
 
     def forward(self, x):
         return self.layer_stack(x)
-
-
-torch.manual_seed(42)
-
-model = FashionMNIST(
-    input_shape=784, hidden_units=10, output_shape=len(class_names)
-).to(device)
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.001, weight_decay=0.01)
 
 
 def print_train_time(start, end, device):
@@ -130,31 +121,8 @@ def test_step(
         test_acc /= len(data_loader)
         print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
 
+        return test_loss, test_acc
 
-torch.manual_seed(42)
-train_time_start_on_gpu = timer()
-
-# epochs = 50
-# for epoch in tqdm(range(epochs)):
-#     print(f"Epoch: {epoch}\n---------")
-#     train_step(
-#         data_loader=train_dataloader,
-#         model=model,
-#         loss_fn=loss_fn,
-#         optimizer=optimizer,
-#         accuracy_fn=accuracy_fn,
-#     )
-#     test_step(
-#         data_loader=test_dataloader,
-#         model=model,
-#         loss_fn=loss_fn,
-#         accuracy_fn=accuracy_fn,
-#     )
-
-train_time_end_on_gpu = timer()
-total_train_time_model_1 = print_train_time(
-    start=train_time_start_on_gpu, end=train_time_end_on_gpu, device=device
-)
 
 torch.manual_seed(42)
 
@@ -185,11 +153,6 @@ def eval_model(
     }
 
 
-model_results = eval_model(
-    model=model, data_loader=test_dataloader, loss_fn=loss_fn, accuracy_fn=accuracy_fn
-)
-
-
 class CNNFasionMNIT(nn.Module):
     def __init__(self, input_shape, hidden_units, output_shape):
         super().__init__()
@@ -201,6 +164,7 @@ class CNNFasionMNIT(nn.Module):
                 padding=1,
                 kernel_size=3,
             ),
+            nn.BatchNorm2d(hidden_units),
             nn.ReLU(),
             nn.Conv2d(
                 in_channels=hidden_units,
@@ -209,6 +173,7 @@ class CNNFasionMNIT(nn.Module):
                 padding=1,
                 kernel_size=3,
             ),
+            nn.BatchNorm2d(hidden_units),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -220,6 +185,7 @@ class CNNFasionMNIT(nn.Module):
                 kernel_size=3,
                 stride=1,
             ),
+            nn.BatchNorm2d(hidden_units),
             nn.ReLU(),
             nn.Conv2d(
                 in_channels=hidden_units,
@@ -228,12 +194,14 @@ class CNNFasionMNIT(nn.Module):
                 kernel_size=3,
                 stride=1,
             ),
+            nn.BatchNorm2d(hidden_units),
             nn.ReLU(),
             nn.MaxPool2d(stride=2, kernel_size=2),
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
+            nn.Dropout(p=0.25),
             nn.Linear(in_features=hidden_units * 7 * 7, out_features=output_shape),
         )
 
@@ -247,5 +215,71 @@ class CNNFasionMNIT(nn.Module):
 
 torch.manual_seed(42)
 model_2 = CNNFasionMNIT(
-    input_shape=1, hidden_units=10, output_shape=len(class_names)
+    input_shape=1, hidden_units=128, output_shape=len(class_names)
 ).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.AdamW(params=model_2.parameters(), lr=0.001, weight_decay=0.01)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, mode="min", patience=5, factor=0.1
+)
+torch.manual_seed(42)
+train_time_start_model_2 = timer()
+# epochs = 50
+# for epoch in tqdm(range(epochs)):
+#     print(f"Epoch: {epoch}\n---------")
+#     train_step(
+#         data_loader=train_dataloader,
+#         model=model_2,
+#         loss_fn=loss_fn,
+#         optimizer=optimizer,
+#         accuracy_fn=accuracy_fn,
+#         device=device,
+#     )
+#     test_loss, test_acc = test_step(
+#         data_loader=test_dataloader,
+#         model=model_2,
+#         loss_fn=loss_fn,
+#         accuracy_fn=accuracy_fn,
+#         device=device,
+#     )
+#     print(f"Current LR: {optimizer.param_groups[0]['lr']}")
+#     scheduler.step(test_loss)
+
+train_time_end_model_2 = timer()
+# total_train_time_model_2 = print_train_time(
+#     start=train_time_start_model_2, end=train_time_end_model_2, device=device
+# )
+
+
+def predict_single_image(model, dataset, classes):
+    # 1. Grab a random image from the test dataset
+    random_idx = random.randint(0, len(dataset) - 1)
+    img, true_label_idx = dataset[random_idx]
+
+    # 2. Prepare the image for the model
+    # Turn on "Eval Mode" (Deactivates Dropout, locks BatchNorm)
+    model.eval()
+
+    with torch.inference_mode():
+        img = img.to(device)
+        img_batch = img.unsqueeze(dim=0)
+        logits = model(img_batch)
+        pred_probs = torch.softmax(logits, dim=1)
+        pred_label_idx = torch.argmax(pred_probs, dim=1).item()
+
+    img_to_plot = img.cpu().squeeze()
+    plt.imshow(img_to_plot, cmap="gray")
+
+    title_color = "green" if pred_label_idx == true_label_idx else "red"
+
+    plt.title(
+        f"True: {classes[true_label_idx]} | Pred: {classes[pred_label_idx]}",
+        color=title_color,
+    )
+    plt.axis("off")
+    plt.show()
+
+
+class_names = train_data.classes
+predict_single_image(model_2, test_data, class_names)
