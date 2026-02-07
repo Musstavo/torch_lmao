b8e9c2efc09033ec50fbef7188c641993855cc71
Musstavo <mohdtantour@gmail.com>
2026-02-07 21:30:36 +0300

    hmm

--- LiverDetection/Liver.py
+++ LiverDetection/Liver.py
@@ -1,6 +1,37 @@
 import os
 import glob
 from natsort import natsorted
+import torch
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from monai.utils import first, set_determinism
+from monai.transforms import (
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    RandAffined,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    Invertd,
+)
+from monai.handlers.utils import from_engine
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.metrics import DiceMetric
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
+from monai.config import print_config
+from monai.apps import download_and_extract
+import tempfile
+import shutil
 
 train_dir = "LiverDetection/data/liver-tumor-segmentation/volume"
 label_dir = "LiverDetection/data/liver-tumor-segmentation/segmentations"
@@ -14,9 +45,69 @@ data_dicts = [
     for image_name, label_name in zip(train_images, train_labels)
 ]
 
-print(f"Found {len(data_dicts)} pairs")
-print(f"First pair: {data_dicts[0]}")
-print(f"Last pair: {data_dicts[-1]}")
+random.shuffle(data_dicts)
+val_size = 20
+train_files, val_files = data_dicts[:-val_size], data_dicts[-val_size:]
+set_determinism(seed=0)
 
+train_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.5, 1.5, 2.0),
+            mode=("bilinear", "nearest"),
+        ),
+        ScaleIntensityRanged(
+            keys=["image"],
+            a_min=-90,
+            a_max=170,
+            b_min=0.0,
+            b_max=1.0,
+            clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        RandCropByPosNegLabeld(
+            keys=["image", "label"],
+            label_key="label",
+            spatial_size=(96, 96, 96),
+            pos=1,
+            neg=1,
+            num_samples=4,
+            image_key="image",
+            image_threshold=0,
+        ),
+        RandAffined(
+            keys=["image", "label"],
+            mode=("bilinear", "nearest"),
+            prob=0.7,
+            spatial_size=(96, 96, 96),
+            rotate_range=(0, 0, np.pi / 15),
+            scale_range=(0.1, 0.1, 0.1),
+        ),
+    ]
+)
 
-print(f"Middle pair: {data_dicts[65]}")
+val_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        ScaleIntensityRanged(
+            keys=["image"],
+            a_min=-90,
+            a_max=170,
+            b_min=0.0,
+            b_max=1.0,
+            clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.5, 1.5, 2.0),
+            mode=("bilinear", "nearest"),
+        ),
+    ]
+)
