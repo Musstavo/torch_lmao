764d98fbceb5086cfbac2d558bb8df0ebfa4e480
Musstavo <mohdtantour@gmail.com>
2026-02-08 22:28:08 +0300

    wdym my gpu can't handle anything :(

--- LiverDetection/Liver.py
+++ LiverDetection/Liver.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from natsort import natsorted
+import tqdm
 import torch
 import random
 import numpy as np
@@ -32,6 +33,8 @@ from monai.config import print_config
 from monai.apps import download_and_extract
 import tempfile
 import shutil
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 train_dir = "LiverDetection/data/liver-tumor-segmentation/volume"
 label_dir = "LiverDetection/data/liver-tumor-segmentation/segmentations"
@@ -111,3 +114,77 @@ val_transforms = Compose(
         ),
     ]
 )
+
+train_ds = Dataset(data=train_files, transform=train_transforms)
+trian_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+
+val_ds = Dataset(data=val_files, transform=val_transforms)
+val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=4)
+
+model = UNet(
+    spatial_dims=3,
+    in_channels=1,
+    out_channels=2,
+    channels=(16, 32, 64, 128, 256),
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+    norm=Norm.BATCH,
+).to(device)
+
+loss_fn = DiceLoss(to_onehot_y=True, softmax=True)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+dice_metric = DiceMetric(include_background=False)
+
+epochs = 100
+val_interval = 5
+best_metric = -1
+best_metric_epoch = -1
+epoch_loss_values = []
+metric_values = []
+
+post_pred = AsDiscrete(argmax=True, to_onehot=3)
+post_label = AsDiscrete(to_onehot=3)
+
+
+for epoch in tqdm(range(epochs)):
+    print(f"epoch {epoch + 1}/{epochs}")
+    model.train()
+    epoch_loss = 0
+    batches_trained = 0
+
+    for batch_data in train_loader:
+        batches_trained += 1
+        images, labels = batch_data["image"].to(device), batch_data["label"].to(device)
+
+        optimizer.zero_grad()
+        preds = model(inputs)
+        loss = loss_fn(preds, labels)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        print(
+            f"{batches_trained}/{len(train_loader)}, train_loss: {loss.item():.4f}",
+            end="\r",
+        )
+
+    epoch_loss /= batches_trained
+    epoch_loss_values.append(epoch_loss)
+    print(f"\nepoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.inference_mode():
+            for val_data in val_loader:
+                val_images, val_labels = (
+                    val_data["images"].to(device),
+                    val_data["label"].to(device),
+                )
+            region_of_interest_size = (96, 96, 96)
+            sliding_window_batch_size = 4
+
+            val_preds = sliding_window_inference(
+                val_images, region_of_interest_size, sliding_window_batch_size, model
+            )
+            val_preds = [post_pred(i) for i in decollate_batch(val_preds)]
+            val_labels = [post_label(i) for i in decollate_batch(val_labels)]
